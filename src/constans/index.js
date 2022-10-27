eb3a0b70d61f1cc65a38e61096c14481adb80d3d
NestorSanchez <nesaenz.69@gmail.com>
2022-10-27 13:32:37 -0500

    add new changes in Formik Contactus

--- src/constans/index.js
+++ src/constans/index.js
@@ -1,6 +1,23 @@
 export const SERVICES = [
-  { id: 1, name: "Uso residencial / Negocio" },
-  { id: 2, name: "Servicio Internet Dedicado" },
-  { id: 3, name: "Camaras Seguridad" },
-  { id: 4, name: "Deseo accesorio tecnologico" },
+  {
+    id: 1,
+    value: " ",
+    name: "--Escoge una opción--",
+  },
+  {
+    id: 2,
+    value: "Uso residencial / Negocio",
+    name: "Uso residencial / Negocio",
+  },
+  {
+    id: 3,
+    value: "Servicio Internet Dedicado",
+    name: "Servicio Internet Dedicado",
+  },
+  { id: 4, value: "Camaras Seguridad", name: "Camaras Seguridad" },
+  {
+    id: 5,
+    value: "Deseo accesorio tecnologico",
+    name: "Deseo accesorio tecnologico",
+  },
 ];
--- src/pages/ContactUs.jsx
+++ src/pages/ContactUs.jsx
@@ -1,20 +1,25 @@
 import React from "react";
-import { useFormik } from "formik";
+import { Formik } from "formik";
 import { SERVICES } from "../constans";
 import { HeaderPage } from "../components/HeaderPage";
+import { formContactusSchema } from "../schemas";
+
+const onSubmit = async (values, actions) => {
+  console.log(values);
+  console.log(actions);
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+  actions.resetForm();
+};
 
 export const ContactUs = () => {
-  const { values, handleChange, handleBur, handleSubmit } = useFormik({
-    initialValues: {
-      servi: "",
-      address: "",
-      name: "",
-      phone: "",
-      email: "",
-      message: "",
-    },
-    onSubmit: (values) => console.log(values),
-  });
+  // const {  } =
+  //   useFormik({
+  //     initialValues: {},
+  //     validationSchema: formContactusSchema,
+  //     onSubmit,
+  //   });
+  // console.log(errors);
+
   return (
     <>
       <HeaderPage message="Contactenos" />
@@ -27,154 +32,198 @@ export const ContactUs = () => {
                 <span className="text-uppercase">Conectate ahora</span>
               </h5>
               <div className="divider-50 d-none d-xl-block"></div>
-              <form
-                onSubmit={handleSubmit}
-                className="contact-form c-mb-10 c-mb-md-20 c-gutter-20 text-center text-md-left"
-                action="/"
+              <Formik
+                initialValues={{
+                  servi: "",
+                  address: "",
+                  name: "",
+                  phone: "",
+                  email: "",
+                  message: "",
+                }}
+                validationSchema={formContactusSchema}
+                onSubmit={onSubmit}
               >
-                <div className="row">
-                  <div className="col-sm-6">
-                    <div className="form-group has-placeholder">
-                      <label htmlFor="select">Seleccione</label>
-                      <i className="fa fa-gear color-main"></i>
-                      <select
-                        className="form-control"
-                        id="select"
-                        name="servi"
-                        onChange={handleChange}
-                        value={values.servi}
-                      >
-                        {SERVICES.map((service) => (
-                          <option key={service.id}>{service.name}</option>
-                        ))}
-                      </select>
+                {({
+                  isSubmitting,
+                  touched,
+                  handleSubmit,
+                  errors,
+                  getFieldProps,
+                }) => (
+                  <form
+                    onSubmit={handleSubmit}
+                    className="contact-form c-mb-10 c-mb-md-20 c-gutter-20 text-center text-md-left"
+                    action="/"
+                  >
+                    <div className="row">
+                      <div className="col-sm-6">
+                        <div className="form-group has-placeholder">
+                          <label htmlFor="select">Seleccione</label>
+                          <i className="fa fa-gear color-main"></i>
+                          <select
+                            className={`form-control ${
+                              errors.servi && touched.servi ? "input-error" : ""
+                            }`}
+                            id="select"
+                            {...getFieldProps("servi")}
+                          >
+                            {SERVICES.map((service) => (
+                              <option value={service.value} key={service.id}>
+                                {service.name}
+                              </option>
+                            ))}
+                          </select>
+                          {errors.servi && touched.servi && (
+                            <p className="error">{errors.address}</p>
+                          )}
+                        </div>
+                      </div>
+                      <div className="col-sm-6">
+                        <div className="form-group has-placeholder">
+                          <label htmlFor="address">Direccón</label>
+                          <i className="fa fa-home color-main"></i>
+                          <input
+                            type="text"
+                            aria-required="true"
+                            size="30"
+                            id="address"
+                            placeholder="Dirección"
+                            className={`form-control ${
+                              errors.address && touched.address
+                                ? "input-error"
+                                : ""
+                            }`}
+                            {...getFieldProps("address")}
+                          />
+                          {errors.address && touched.address && (
+                            <p className="error">{errors.address}</p>
+                          )}
+                        </div>
+                      </div>
                     </div>
-                  </div>
-                  <div className="col-sm-6">
-                    <div className="form-group has-placeholder">
-                      <label htmlFor="address">Direccón</label>
-                      <i className="fa fa-home color-main"></i>
-                      <input
-                        type="text"
-                        aria-required="true"
-                        size="30"
-                        name="address"
-                        id="address"
-                        className="form-control"
-                        placeholder="Dirección"
-                        onChange={handleChange}
-                        value={values.address}
-                      />
-                    </div>
-                  </div>
-                </div>
 
-                <div className="row">
-                  <div className="col-sm-6">
-                    <div className="form-group has-placeholder">
-                      <label htmlFor="name">Full Name </label>
-                      <i className="fa fa-user color-main"></i>
-                      <input
-                        type="text"
-                        aria-required="true"
-                        size="30"
-                        name="name"
-                        id="name"
-                        className="form-control"
-                        placeholder="Nombres"
-                        onChange={handleChange}
-                        value={values.name}
-                      />
+                    <div className="row">
+                      <div className="col-sm-6">
+                        <div className="form-group has-placeholder">
+                          <label htmlFor="name">Full Name </label>
+                          <i className="fa fa-user color-main"></i>
+                          <input
+                            type="text"
+                            aria-required="true"
+                            size="30"
+                            id="name"
+                            placeholder="Nombres"
+                            className={`form-control ${
+                              errors.name && touched.name ? "input-error" : ""
+                            }`}
+                            {...getFieldProps("name")}
+                          />
+                          {errors.name && touched.name && (
+                            <p className="error">{errors.name}</p>
+                          )}
+                        </div>
+                      </div>
+                      <div className="col-sm-6">
+                        <div className="form-group has-placeholder">
+                          <label htmlFor="phone">Phone Number</label>
+                          <i className="fa fa-phone color-main"></i>
+                          <input
+                            type="text"
+                            aria-required="true"
+                            size="30"
+                            id="phone"
+                            className={`form-control ${
+                              errors.phone && touched.phone ? "input-error" : ""
+                            }`}
+                            placeholder="Numero celular"
+                            {...getFieldProps("phone")}
+                          />
+                          {errors.phone && touched.phone && (
+                            <p className="error">{errors.phone}</p>
+                          )}
+                        </div>
+                      </div>
                     </div>
-                  </div>
-                  <div className="col-sm-6">
-                    <div className="form-group has-placeholder">
-                      <label htmlFor="phone">Phone Number</label>
-                      <i className="fa fa-phone color-main"></i>
-                      <input
-                        type="text"
-                        aria-required="true"
-                        size="30"
-                        name="phone"
-                        id="phone"
-                        className="form-control"
-                        placeholder="Numero celular"
-                        onChange={handleChange}
-                        value={values.phone}
-                      />
-                    </div>
-                  </div>
-                </div>
 
-                <div className="row">
-                  <div className="col-sm-12">
-                    <div className="form-group has-placeholder">
-                      <label htmlFor="email">Email address</label>
-                      <i className="fa fa-envelope color-main"></i>
-                      <input
-                        type="email"
-                        aria-required="true"
-                        size="30"
-                        name="email"
-                        id="email"
-                        className="form-control"
-                        placeholder="E-mail"
-                        onChange={handleChange}
-                        value={values.email}
-                      />
-                    </div>
-                  </div>
+                    <div className="row">
+                      <div className="col-sm-12">
+                        <div className="form-group has-placeholder">
+                          <label htmlFor="email">Email address</label>
+                          <i className="fa fa-envelope color-main"></i>
+                          <input
+                            type="email"
+                            aria-required="true"
+                            size="30"
+                            id="email"
+                            className={` ${
+                              errors.email && touched.email ? "input-error" : ""
+                            } form-control`}
+                            placeholder="E-mail"
+                            {...getFieldProps("email")}
+                          />
+                          {errors.email && touched.email && (
+                            <p className="error">{errors.email}</p>
+                          )}
+                        </div>
+                      </div>
 
-                  <div className="col-sm-12">
-                    <div className="form-group has-placeholder">
-                      <label htmlFor="message">Message</label>
-                      <i className="fa fa-edit color-main"></i>
-                      <textarea
-                        aria-required="true"
-                        rows="6"
-                        cols="45"
-                        name="message"
-                        id="message"
-                        className="form-control"
-                        placeholder="Tu mensaje"
-                        onChange={handleChange}
-                        value={values.message}
-                      ></textarea>
+                      <div className="col-sm-12">
+                        <div className="form-group has-placeholder">
+                          <label htmlFor="message">Message</label>
+                          <i className="fa fa-edit color-main"></i>
+                          <textarea
+                            aria-required="true"
+                            rows="6"
+                            cols="45"
+                            id="message"
+                            className={`form-control ${
+                              errors.message && touched.message
+                                ? "input-error"
+                                : ""
+                            }`}
+                            placeholder="Tu mensaje"
+                            {...getFieldProps("message")}
+                          ></textarea>
+                        </div>
+                      </div>
                     </div>
-                  </div>
-                </div>
-                <div className="row">
-                  <div className="col-sm-12">
-                    <div className="form-check">
-                      <input
-                        type="checkbox"
-                        className="form-check-input"
-                        id="exampleCheck1"
-                      />
-                      <label
-                        className="form-check-label"
-                        htmlFor="exampleCheck1"
-                      >
-                        Acepto que mis datos enviados se recopilen y almacenen.
-                      </label>
+                    <div className="row">
+                      <div className="col-sm-12">
+                        <div className="form-check">
+                          <input
+                            type="checkbox"
+                            className="form-check-input"
+                            id="exampleCheck1"
+                          />
+                          <label
+                            className="form-check-label"
+                            htmlFor="exampleCheck1"
+                          >
+                            Acepto que mis datos enviados se recopilen y
+                            almacenen.
+                          </label>
+                        </div>
+                      </div>
                     </div>
-                  </div>
-                </div>
-                <div className="row">
-                  <div className="col-sm-12 text-center text-md-left mt-30">
-                    <div className="form-group">
-                      <button
-                        type="submit"
-                        id="contact_form_submit"
-                        name="contact_submit"
-                        className="btn btn-maincolor"
-                      >
-                        <span>Enviar</span>
-                      </button>
+                    <div className="row">
+                      <div className="col-sm-12 text-center text-md-left mt-30">
+                        <div className="form-group">
+                          <button
+                            type="submit"
+                            id="contact_form_submit button_submit-disabled"
+                            name="contact_submit"
+                            className="btn btn-maincolor"
+                            disabled={isSubmitting}
+                          >
+                            <span>Enviar</span>
+                          </button>
+                        </div>
+                      </div>
                     </div>
-                  </div>
-                </div>
-              </form>
+                  </form>
+                )}
+              </Formik>
             </div>
           </div>
         </div>
