6eb71955d07a717e83946a897fb7eb5b67057c71
NestorSanchez <nesaenz.69@gmail.com>
2022-12-30 11:48:13 -0500

    change domain. add https en -env

--- src/constans/dominio.js
+++ src/constans/dominio.js
@@ -1,4 +1,4 @@
-export const URL_BASE = `http://${import.meta.env.VITE_DOMAIN}/api/v1/`;
+export const URL_BASE = `${import.meta.env.VITE_DOMAIN}/api/v1/`;
 
 //Products
-export const URL_BASE2 = `http://${import.meta.env.VITE_DOMAIN}`;
+export const URL_BASE2 = `${import.meta.env.VITE_DOMAIN}`;
